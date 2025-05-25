43d2bdbb5b3c41a21d06b55da2fdc4cdce998487
ravidu_senevirathne <senevirathneravidu@gmail.com>
2025-05-25 23:09:22 +0530

    added tailwind

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section>
+      {/* Hero content will go here */}
+      <p>Hero Section</p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      {/* Navbar content will go here */}
+      <p>Navbar</p>
+    </nav>
+  );
+}
+
+export default Navbar;
